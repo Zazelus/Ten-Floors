7a8cf3e393dc559eaf8fa2016d8ccc3f23bfd007
Zazelus <55807999+Zazelus@users.noreply.github.com>
2020-04-20 15:45:41 -0400

    Created FloorParser
    Will take an existing dungeon floor and depending on the strings included, will generate a correspong object to be interacted with. Each indexed location is a room.

---
+++ Ten Floors/src/com/tenfloors/dungeon/FloorParser.java
@@ -0,0 +1,15 @@
+package com.tenfloors.dungeon;
+
+public class FloorParser {
+
+	private String[][] dungeonFloor;
+
+	public FloorParser(String[][] dungeonFloor) {
+		this.dungeonFloor = dungeonFloor;
+	}
+
+	public void parseFloorData() {
+
+	}
+
+}
